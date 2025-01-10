e08773a4f20ef643e205f1e9dbd159d5bcdb9973
Andrey Allyson <andreyuser47@gmail.com>
2025-01-10 16:03:18 -0300

    solution 3

--- teste-3.js
+++ teste-3.js
@@ -6,3 +6,56 @@
 // IMPORTANTE:
 // a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 // b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
+
+const fs = require("fs");
+
+const buff = fs.readFileSync("teste-3-fat.json").toString("utf8");
+/**@type { Array< { dayIndex: number; sales: number; } > } */
+const days = JSON.parse(buff);
+
+let minSalesDay = null;
+let maxSalesDay = null;
+let monthSales = 0;
+
+for (const day of days) {
+  if (minSalesDay == null || day.sales < minSalesDay.sales) {
+    minSalesDay = day;
+  }
+
+  if (maxSalesDay == null || day.sales > maxSalesDay.sales) {
+    maxSalesDay = day;
+  }
+
+  monthSales += day.sales;
+}
+
+const monthAvg = monthSales / days.length;
+
+const numOfDaysAboveAvg = days.reduce(
+  (acc, day) => (day.sales > monthAvg ? acc + 1 : acc),
+  0
+);
+
+console.log(
+  `O dia ${
+    minSalesDay.dayIndex + 1
+  } teve o menor faturamento do mês, faturamento esse de ${toCurrencyString(
+    minSalesDay.sales
+  )}`
+);
+console.log(
+  `O dia ${
+    maxSalesDay.dayIndex + 1
+  } teve o maior faturamento do mês, faturamento esse de ${toCurrencyString(
+    maxSalesDay.sales
+  )}`
+);
+console.log(
+  `E um total de ${numOfDaysAboveAvg} dias tiveram o seu faturamento acima da média mensal de ${toCurrencyString(
+    monthAvg
+  )}`
+);
+
+function toCurrencyString(num) {
+  return `R$ ${num.toFixed(2).replace(".", ",")}`;
+}
